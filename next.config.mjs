940a873fdcc4cc1ea59673aa3ff9cd451d2cf9da
Brant <itsbrantttv@gmail.com>
2025-02-26 06:49:56 -0500

    Update config to understand where aliases are during build.

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,14 @@ const nextConfig = {
   distDir: 'out',
   experimental: {
     missingSuspenseWithCSRBailout: false
-  }
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '.',
+    };
+    return config;
+  },
 }
 
 export default nextConfig 
